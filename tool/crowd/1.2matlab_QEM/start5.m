5a64038da397bf8815e6c956d766e6a6deaba62b
bigOrange123456789 <bigOrange123456789>
2022-11-25 19:31:53 +0800

    基本完成了PM的所有代码

--- tool/crowd/1.2matlab_QEM/start5.m
+++ tool/crowd/1.2matlab_QEM/start5.m
@@ -3,6 +3,6 @@ addpath('src');
 addpath('lib/jsonlab-master');
 
 %Group.process_save('manA20_2.json',0.99,3);
-Group.process_save('manA20.json',0.5,20000);
+Group.process_save('manA20.json',0.001,20);
 %Group.process_save('manA20.json',0.01,20);
 %Group.process_save('test2.json',0.5,4);
