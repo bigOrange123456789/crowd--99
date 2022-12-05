3fced8bcbd583411bcc23a052c53106231681688
bigOrange123456789 <bigOrange123456789>
2022-12-05 21:26:01 +0800

    LOD的bin包可以解析出mesh名

--- tool/crowd/writebin/compressPm2.py
+++ tool/crowd/writebin/compressPm2.py
@@ -3,8 +3,18 @@ import os
 from tqdm import tqdm
 import numpy as np
 import struct
-
-
+def remove(dir_path):
+    import os
+    # os.walk会得到dir_path下各个后代文件夹和其中的文件的三元组列表，顺序自内而外排列，如 o下有1文件夹，1下有2文件夹：[('o\1\2', [], ['a.py','b']), ('o\1', ['2'], ['c']), ('o', ['1'], [])]
+    for root, dirs, files in os.walk(dir_path, topdown=False):
+        #root: 各级文件夹绝对路径
+        #dirs: root下一级文件夹名称列表，如 ['文件夹1','文件夹2']
+        #files: root下文件名列表，如 ['文件1','文件2']
+        for name in files:# 第一步：删除文件
+            os.remove(os.path.join(root, name))  # 删除文件
+        for name in dirs:# 第二步：删除空文件夹
+            os.rmdir(os.path.join(root, name)) # 删除一个空目录
+            
 # 将list转化为Float32二进制
 def listToBin32(data):
     data_bin = []
@@ -13,35 +23,24 @@ def listToBin32(data):
         data_bin.append(b)
     return data_bin
 
-
-# 将list转化为Float16二进制
-def listToBin16(data):
-    data_bin = []
-    for item in data:
-        b = struct.pack('<e', float(item))
-        data_bin.append(b)
-    return data_bin
-
-
-def floatToBin32(data):
-    b = struct.pack('<f', float(data))
-    return b
-
-
 # 保存二进制文件
 def storeBin(path, data):
     with open(path, 'wb') as f:
         for item in data:
             f.write(item)
 
-
 # 将图像保存为二进制
-def saveImgBin(head, img, img2, img3, path):
-    data = []
+# def saveImgBin(head, img, img2, img3, path):
+def saveMesh(opt,data):
+    head=opt["head"]
+    img=opt["img"] 
+    img2=opt["img2"]  
+    img3=opt["img3"]  
+    # path=opt["path"]
+
+    # data = []
     for e in head:
         data.append(struct.pack('<I', e))
-    # data.append(struct.pack('<I',head[1]))
-    # data.append(struct.pack('<I',head[2]))
     data += listToBin32(
         np.array(img).reshape(-1)
     )
@@ -51,25 +50,37 @@ def saveImgBin(head, img, img2, img3, path):
     data += listToBin32(
         np.array(img3).reshape(-1)
     )
+    # storeBin(path, data)
+    return data
+
+def getMeshLength(mesh):
+    img=mesh["img"] 
+    img2=mesh["img2"]  
+    img3=mesh["img3"]  
+    # path=opt["path"]
+    l0=4
+    l1=np.array(img).reshape(-1).shape[0]
+    l2=np.array(img2).reshape(-1).shape[0] 
+    l3=np.array(img3).reshape(-1).shape[0]
+    # print(l0,l1,l2,l3,np.array(img).reshape(-1).shape)
+    # storeBin(path, data)
+    return l0+l1+l2+l3
+
+def saveMeshes(path,meshes):
+    data=[]
+    for mesh in meshes:
+        getMeshLength(mesh)
+        saveMesh(mesh,data)
     storeBin(path, data)
 
-
-if __name__ == "__main__":
-    path = "1.json.pack.json"
-    print("数据的路径为:", path)
-    file = open(path)
-    result = json.load(file)
-    # print("图片1的长度:",np.array(result["animation"]).reshape(-1).shape)#逆矩阵与世界矩阵
-    # print("图片2的长度:",np.array(result["animation2"]).reshape(-1).shape)#逆矩阵与世界矩阵的乘积
-
-    for meshname, value in result.items():
-        # listSim是啥不明白
-        if len(value) > 0 and meshname != 'listSim' and meshname != 'names':
+def getMesh(value):
             array1 = []
             array2 = []  # 表示faceRe
             array3 = []  # 表示face
             array2length = 0
             array3length = 0
+            if type(value)==type({}):
+                value=[[value]]
             for i in value[0]:
                 # 处理array1
                 array1.append(i['aI'])
@@ -77,34 +88,44 @@ if __name__ == "__main__":
                 array1.append(i['aPos'][0])
                 array1.append(i['aPos'][1])
                 array1.append(i['aPos'][2])
+
                 array1.append(i['bPos'][0])
                 array1.append(i['bPos'][1])
                 array1.append(i['bPos'][2])
+
                 array1.append(i['cPos'][0])
                 array1.append(i['cPos'][1])
                 array1.append(i['cPos'][2])
+
                 array1.append(i['aUV'][0])
                 array1.append(i['aUV'][1])
+
                 array1.append(i['aSkinWeight'][0])
                 array1.append(i['aSkinWeight'][1])
                 array1.append(i['aSkinWeight'][2])
                 array1.append(i['aSkinWeight'][3])
+
                 array1.append(i['aSkinIndex'][0])
                 array1.append(i['aSkinIndex'][1])
                 array1.append(i['aSkinIndex'][2])
                 array1.append(i['aSkinIndex'][3])
+
                 array1.append(i['bUV'][0])
                 array1.append(i['bUV'][1])
+
                 array1.append(i['bSkinWeight'][0])
                 array1.append(i['bSkinWeight'][1])
                 array1.append(i['bSkinWeight'][2])
                 array1.append(i['bSkinWeight'][3])
+
                 array1.append(i['bSkinIndex'][0])
                 array1.append(i['bSkinIndex'][1])
                 array1.append(i['bSkinIndex'][2])
                 array1.append(i['bSkinIndex'][3])
+
                 array1.append(array2length)
                 array1.append(array3length)
+
                 if isinstance(i['faceRe'],list):
                     array2length += len(i['faceRe'])
                     array1.append(len(i['faceRe']))
@@ -114,6 +135,7 @@ if __name__ == "__main__":
                     array2length += 1
                     array1.append(1)
                     array2.append(i['faceRe'])
+                    
                 if isinstance(i['face']['x'], list):
                     array3length += len(i['face']['x']) * 4
                     array1.append(len(i['face']['x']))
@@ -129,27 +151,45 @@ if __name__ == "__main__":
                     array3.append(i['face']['y'])
                     array3.append(i['face']['z'])
                     array3.append(i['face']['d'])
-            print(array1, array2, array3)
-            saveImgBin([
-                len(value[0]), # 这个mesh中变化的个数
-                len(array1), #array1长度
-                array2length,
-                array3length
-            ],
-            array1,
-            array2,
-            array3,
-            "pack/"+meshname+'.bin')
+            return {
+                "head":[
+                    len(value[0]), # 这个mesh中变化的个数
+                    len(array1), #array1长度
+                    array2length,
+                    array3length
+                ],
+                "img" :array1,
+                "img2":array2,
+                "img3":array3
+                # "path":path#outpath+fileName+"/"+meshname+'.bin'
+            }
 
-    # print(array1,array2,array3)
-    # saveImgBin(
-    #     [
-    #         len(result["config"]),  # 动画的个数
-    #         result["config"][0],  # 单个动画数据的长度
-    #         np.array(result["animation"]).reshape(-1).shape[0],  # 图片1的长度
-    #         int(result["frameNumber"])
-    #     ],
-    #     result["animation"],
-    #     result["animation2"],
-    #     "test.bin"
-    # )
+def process(index):
+    name = str(index)+".json.pack.json"
+    inpath = "in/"+name
+    outpath="out/"
+    
+    print("输入数据路径:", inpath)
+    file = open(inpath)
+    result = json.load(file)
+    
+    fileName=name.split(".json.pack.json")[0]
+    # os.mkdir(outpath+fileName)
+
+    meshes=[]
+    for meshname, value in result.items():
+        # listSim是啥不明白
+        if len(value) > 0 and meshname != 'listSim' and meshname != 'names':
+            mesh=getMesh(value)
+            meshes.append(mesh)
+    saveMeshes(
+        outpath+fileName+'.bin',
+        meshes
+    )
+
+if __name__ == "__main__":
+    remove("out/")
+    # os.mkdir("out/")
+    for i in range(19):
+        process(i+1)
+    # process(19)
